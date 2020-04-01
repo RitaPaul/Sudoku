73b6bc402025c0adf521eaf3c9c9a0c34df1fff9
RitaPaul <rpa@sportcompass.dk>
2020-04-01 20:36:49 +0200

    new board

--- play.js
+++ play.js
@@ -43,6 +43,7 @@ for (var i=0; i< sud.length;i++){
 let Sudoku = function(start,end){
    for( i = 0; i < start; i ++){
        for(j = 0; j < end; j ++ ){
+       // document.write(j);
        document.write(sud[i][j]);
        document.write( ' &nbsp; ');
        if(j == 2 || j == 5  ){
@@ -68,27 +69,40 @@ let newBoard = function(){
    let j2 = Random_num(6,8);
 
 
-   let j3 = Random_num(1,3);
-   let j4 = Random_num(4,6);
+   let j3 = Random_num(1,2);
+   let j4 = Random_num(4,5);
    let j5 = Random_num(7,8);
+
+
 
     
 
-   console.log(sud[1][j0] = Random_num(1,3));
+   console.log(sud[0][j0] = Random_num(1,3));
    console.log(sud[0][j1] = Random_num(4,6));
-   console.log(sud[2][j2] = Random_num(7,9));
+   console.log(sud[0][j2] = Random_num(7,9));
 
    console.log(sud[2][j3] = Random_num(7,9));
    console.log(sud[2][j4] = Random_num(1,3));
    console.log(sud[2][j5] = Random_num(4,5));
 
-   console.log(sud[4][j1] = Random_num(2,4));
-   console.log(sud[5][j2] = Random_num(5,7));
-   console.log(sud[4][j0] = Random_num(8,9));
+   console.log(sud[3][j1] = Random_num(2,4));
+   console.log(sud[3][j2] = Random_num(5,7));
+   console.log(sud[3][j0] = Random_num(8,9));
+
+   console.log(sud[5][j1] = Random_num(8,9));
+   console.log(sud[5][j2] = Random_num(2,4));
+   console.log(sud[5][j0] = Random_num(5,7));
+
 
    //console.log(sud[0][j2] = Math.floor(Math.random() *  ) + 1;
    //sud[0][j0] =Random_num(0,8);
 
-   console.log(" J0 = "+ j0 + " j1 = " + j1 +" j2 = " + j2);
+   console.log(" J0 =  " + j0);
+   console.log(" j1 =  " + j1);
+   console.log(" j2 =  " + j2);
+   console.log(" j3 =  " + j3);
+   console.log(" j4 =  " + j4);
+   console.log(" j5 =  " + j5);
+
     Sudoku(9,9);
 }
